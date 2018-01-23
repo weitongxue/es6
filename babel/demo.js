091faa234b63b44a21cac801fe7a8c610cfecb19
唐向荣 <641418330@qq.com>
2018-01-23 17:14:46 +0800

    es6源码

---
+++ babel/demo.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+/*let a = 5;
+const b = 10;
+let input = [1,2,3];
+input.map(item => item + 1);*/
---
+++ vuecli/src/common.js
@@ -0,0 +1,24 @@
+/*export function multiply(x,y){
+	return x * y;
+}*/
+// export 1;
+// var m = 1;
+
+// export var m = 1;
+/*var n = 2;
+// export {m};
+export {n as m};*/
+
+/*export default function (){
+	console.log("唐菜也");
+}*/
+
+export default {
+	name:"tangcaiye",
+	id:12324
+}
+
+
+
+
+
